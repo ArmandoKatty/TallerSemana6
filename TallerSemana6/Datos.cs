ab6616a4544729dc57b7a37bbfac4133f0595dcf
user <user@DESKTOP-PAHTI6V>
2022-11-16 09:17:52 -0500

    Agregar archivos de proyecto.

---
+++ TallerSemana6/Datos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TallerSemana6
+{
+    class Datos
+    {
+        public int codigo { get; set; }
+
+
+        public string nombre { get; set; }
+
+        public string estado { get; set; }
+
+         }
+}
---
+++ TallerSemana6/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TallerSemana6
+{
+    public partial class MainPage : ContentPage
+    {
+        private const string URL = "http://172.17.240.1/simov/post.php";
+        private readonly HttpClient client = new HttpClient();
+        private ObservableCollection<Datos> _post;
+        public MainPage()
+        {
+            InitializeComponent();
+            GetData();
+        }
+
+        private async void GetData()
+        {
+            var content = await client.GetStringAsync(URL);
+            List<Datos> post = JsonConvert.DeserializeObject<List<Datos>>(content);
+            _post= new ObservableCollection<Datos>(post);
+            MyListView.ItemsSource = _post;
+        }
+
+        private async void btnIrIngreso_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new VentanaIngreso());
+            //DependencyService.Get<Mensaje>().ShortAlert("mensaje de prueba");
+        }
+
+       
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+           Datos post = _post[0];
+           await client.DeleteAsync($"{URL}?codigo={post.codigo}");
+           _post.Remove(post);
+        }
+
+        private async void btnModificar_Clicked(object sender, EventArgs e)
+        {
+            Datos post = _post[0];
+            post.codigo = _post[0].codigo;
+            post.nombre = _post[0].nombre;
+            post.estado = _post[0].estado;
+            await client.PutAsync($"{URL}?codigo={post.codigo}&nombre={post.nombre}&estado={post.estado}", null);
+            DependencyService.Get<Mensaje>().ShortAlert("Dato actualizado");
+        }
+    }
+}
